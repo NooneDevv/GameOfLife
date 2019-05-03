15263343371d9f6e6a02fc5c9494b18edb8452c6
NooneDevv <xrpfnatic@gmail.com>
2019-05-03 10:22:00 +0200

    Added stopwatch functions and documentation

--- GameOfLifePy/src/StopWatch.py
+++ GameOfLifePy/src/StopWatch.py
@@ -7,8 +7,13 @@ class StopWatch:
         self.start = time.time()
 
     def get_elapsed_time(self):
+        """Returns elapsed time in HH:MM:SS format."""
         return time.strftime("%H:%M:%S", time.gmtime(time.time() - self.start))
 
     def get_elapsed_time_seconds(self):
+        """Returns elapsed time in epoch format."""
         return time.time() - self.start
 
+    def reset(self):
+        """Resets the start time."""
+        self.start = time.time()
